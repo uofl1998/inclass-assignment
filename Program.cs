029463369ec6af697cdcd7dcb88e673c0581416a
uofl1998 <uofl1998@gmail.com>
2025-11-22 11:40:19 -0500

    In class assignment asking about person

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hello Fellow Coder!");
+Thread.Sleep(5000);
+Console.Write("What is your favorite color? ");
+string? color = Console.ReadLine();
+Console.WriteLine($"Your favorite color is {color}. That's amazing!");
+Thread.Sleep(5000);
+Console.Write("Oh dear.. Ha Ha I forgot to ask what your name is?!");
+string? name = Console.ReadLine();
+Thread.Sleep(5000);
+Console.WriteLine($"Oh man it so great to meet you {name} its amazing you favorite color is {color}. We both like the same color how about that!");
+Thread.Sleep(6000);
+Console.WriteLine($"Thanks for your response {name} We'll talk again soon ok!");
